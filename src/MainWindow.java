e5b7c45485bd3067b2b29cf9b14e8fd2333fb07c
Toth-Almos <toth.2000.almos@gmail.com>
2023-07-18 18:07:56 +0200

    Frontend

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
     private JPanel taskList;
@@ -12,6 +14,7 @@ public class MainWindow extends JFrame {
         pack();
         setResizable(false);
         setVisible(true);
+        getContentPane().setBackground(new Color(69, 69, 69));
         setLocationRelativeTo(null);
         setLayout(null);
 
@@ -22,7 +25,54 @@ public class MainWindow extends JFrame {
         this.taskList = new JPanel();
         this.listContainer = new JPanel();
 
-        this.taskList.setLayout(new BoxLayout(taskList, BoxLayout.Y_AXIS));
+        taskList.setLayout(new BoxLayout(taskList, BoxLayout.Y_AXIS));
+        listContainer.add(taskList);
+        listContainer.setBackground(new Color(156, 156, 156));
+
+        //scroll:
+        JScrollPane scrollPane = new JScrollPane(listContainer);
+        scrollPane.setBounds(8, 10, 510, 585);
+        scrollPane.setBorder(BorderFactory.createLoweredBevelBorder());
+        scrollPane.setMaximumSize(new Dimension(510, 585));
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        //add new task button:
+        JButton addNewTaskButton = new JButton("Add Task");
+        addNewTaskButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addNewTaskButton.setBounds(10, 630, 120, 50);
+        addNewTaskButton.setFocusPainted(false);
+        /*addNewTaskButton.setOpaque(false);
+        addNewTaskButton.setBorderPainted(false);
+        addNewTaskButton.setContentAreaFilled(false);
+        addNewTaskButton.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));*/
+
+        //clear finished button:
+        JButton clearButton = new JButton("Clear Finished");
+        clearButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        clearButton.setBounds(140, 630, 120, 50);
+        clearButton.setFocusPainted(false);
+
+        //sort button:
+        JButton sortButton = new JButton("Sort");
+        sortButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        sortButton.setBounds(270, 630, 120, 50);
+        sortButton.setFocusPainted(false);
+
+        //add them to the frame:
+        this.getContentPane().add(addNewTaskButton);
+        this.getContentPane().add(clearButton);
+        this.getContentPane().add(sortButton);
+        this.getContentPane().add(scrollPane);
+
+        //add new task ActionListener:
+        addNewTaskButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO
+                System.out.println("under construction");
+            }
+        });
 
     }
 }
